49f97a27679f25f9fed95e161df4b1bc2fd8ef41
Carlos-ETPI <carlos.anime.000@gmail.com>
2023-05-06 15:37:23 -0600

    pruebas de configuracion

--- Grupo14Discapacitados/Grupo14Discapacitados/config/base.py
+++ Grupo14Discapacitados/Grupo14Discapacitados/config/base.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'Grupo14Discapacitados.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ Grupo14Discapacitados/aplicaciones/estadisticos/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('estadisticos/prueba',views.prueba ),
+]
